0678118bb7efea562ac5e9feb5b3ce3290701564
moskowitza <moskowitzaaron@mac.com>
2018-03-16 11:57:26 -0400

    ALMOST! need to change some dates to number value so we can math the number of months. Also need to make sure the row appends

--- assets/javascript/employee.js
+++ assets/javascript/employee.js
@@ -39,16 +39,23 @@ $("#submit").on("click", function(event) {
     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(childsnapshot) {
       $(".tableBody").empty()
       // moved the moment calculation to down here
+     
       var startMoment = moment(childsnapshot.val().startDate, "MM/DD/YYYY");
-      var monthsWorked = moment().diff(startMoment, "months");
-      var monthsWorkedTd = $("<td>").text(monthsWorked)
-      var total = monthsWorked * parseInt(childsnapshot.val().monthlyRate)
-      console.log(monthsWorked);
+      var monthsWorked = moment("m").diff(startMoment, "m"); //should be a number but isn't
+      var monthsWorkedTd = $("<td>").text(monthsWorked);
+      //totalTd is Total Billed number.  Months Worked x Rate: as long as both are numbers
+      var totalTd = monthsWorked * parseInt(childsnapshot.val().monthlyRate);
+
+      console.log(monthsWorked);  //shoots NAN Error, let's dig 
+      console.log(startMoment); //returns an moment object
+      console.log(childsnapshot.val().startDate); //returns the date entered
+      console.log(totalTd);
       //Check these names. may want to use childsnapshot.val()
       var nameTd = $("<td>").text(childsnapshot.val().name);
       var positionTd = $("<td>").text(childsnapshot.val().position);
       var startDateTd = $("<td>").text(childsnapshot.val().startdate);
-      var monthlyRate=$("<td>").text(childsnapshot.val().monthlyRate);
-      tRow.append(nameTd, positionTd, monthlyRate, startDateTd, monthsWorkedTd);
+
+      var monthlyRateTd=$("<td>").text(childsnapshot.val().monthlyRate);
+      tRow.append(nameTd, positionTd, startDateTd, monthsWorkedTd, monthlyRateTd, totalTd);
       $(".tableBody").append(tRow)
     })
